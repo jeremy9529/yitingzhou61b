89a8ce34b4be5e00b27591e581c6368552d72e3f
yiting zhou <yitingzhou@yitingdeMBP.fios-router.home>
2019-09-12 17:31:20 -0400

    dsfsdfsdf

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,111 +1,103 @@
-
 public class LinkedListDeque<T> {
-	private class Dlist{
-		private T item;
-		private Dlist rest;
-		private Dlist pre;
-		private Dlist(T i, Dlist r,Dlist p){
-			item = i;
-			rest = r;
-			pre = p;
-		}
-	}
-	
-	private int size;
-	private Dlist sentinel;
-	public LinkedListDeque() {
-        sentinel = new Dlist(null,null,null);
-        sentinel.pre = sentinel;
-        sentinel.rest = sentinel;
+    private class StuffNode {
+        private T item;
+        private StuffNode prev;
+        private StuffNode next;
+
+        private StuffNode(T i, StuffNode p, StuffNode n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    private StuffNode sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new StuffNode(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
         size = 0;
     }
-	
-	public void addFirst(T item) {
-		sentinel.rest = new Dlist(item,sentinel, sentinel.rest);
-        sentinel.rest.rest.pre = sentinel.rest;
-        size += 1;
-	}
-	
-	public void addLast(T item) {
-		sentinel.pre = new Dlist(item,sentinel, sentinel.pre);
-		sentinel.pre.pre.rest = sentinel.pre;
-		size += 1;
-	}
-	
-	public boolean isEmpty() {
-		if(size == 0) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public void printDeque() {
-		Dlist temp = sentinel;
-		while(temp.rest.item != null) {
-			System.out.print(temp.rest.item+" ");
-			temp = temp.rest;
-		}
-	}
-    
-	public T removeFirst() {
-		if(size == 0) {
-			return null;
-		}else {
-			T firstitem = sentinel.rest.item;
-			sentinel.rest.rest.pre = sentinel;
-			sentinel.rest = sentinel.rest.rest;
-	        size -= 1;
-	        return firstitem;
-		}
-	}
-	
-	public T removeLast() {
-		if(size == 0) {
-			return null;
-		}else {
-			T lastitem = sentinel.pre.item;
-			sentinel.pre.pre.rest = sentinel;
-			sentinel.pre = sentinel.pre.pre;
-	        size -= 1;
-	        return lastitem;
-		}
-		
-	}
-	
-	public T get(int index) {
-		Dlist temp = sentinel;
-		int i = 0;
-		if(index < 0 || index > this.size()-1) {
-			return null;
-		}else {
-			while(i <= index) {
-				temp = temp.rest;
-				i++;
-			}
-			return temp.item;
-		}
-		
-	}
-	
-	public T getRecursive(int index) {
-		if(index < 0 || index > this.size()-1) {
-			return null;
-		}else {
-			return this.helper(sentinel.rest,index);
-		}
-	}
-	
-	public T helper(Dlist temp, int i) {
-		if(i == 0) {
-			return temp.item;
-		}else {
-			return helper(temp.rest,i-1);
-		}
-	}
 
+    public void addFirst(T item) {
+        size += 1;
+        sentinel.next = new StuffNode(item, sentinel, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+    }
+
+    public void addLast(T item) {
+        size += 1;
+        sentinel.prev.next = new StuffNode(item, sentinel.prev, sentinel);
+        sentinel.prev = sentinel.prev.next;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        StuffNode p = sentinel;
+
+        while (p.next != sentinel) {
+            p = p.next;
+            System.out.print(p.item + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        T i = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        return i;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        T i = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        return i;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index > size - 1) {
+            return null;
+        }
+
+        StuffNode p = sentinel;
+
+        while (index >= 0) {
+            p = p.next;
+            index -= 1;
+        }
+        return p.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index > size - 1) {
+            return null;
+        }
+        return getRecursiveHelper(sentinel.next, index);
+    }
+
+    private T getRecursiveHelper(StuffNode p, int index) {
+        if (index == 0) {
+            return p.item;
+        }
+        return getRecursiveHelper(p.next, index - 1);
+    }
 }
